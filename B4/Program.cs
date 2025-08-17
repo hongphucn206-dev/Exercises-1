265fd3db82ae51bd3eeb0fd2d4ad49134920d984
hongphucn206-dev <hongphucn206@gmail.com>
2025-08-17 09:21:39 +0700

    Add files via upload

---
+++ B4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double feet = double.Parse(Console.ReadLine());
+            Console.WriteLine(feet * 0.3048);
+        }
+    }
+}
---
+++ B5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Celsius → Fahrenheit
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine(c * 9 / 5 + 32);
+
+            // Fahrenheit → Celsius
+            double f = double.Parse(Console.ReadLine());
+            Console.WriteLine((f - 32) * 5 / 9);
+        }
+    }
+}
