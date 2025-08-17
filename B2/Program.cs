5ec48dedc0bf8b9987e1b862db4afc184f5ce5cf
hongphucn206-dev <hongphucn206@gmail.com>
2025-08-17 09:13:36 +0700

    Add files via upload

---
+++ B2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
---
+++ B6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: " + sizeof(int));
+            Console.WriteLine("Size of float: " + sizeof(float));
+            Console.WriteLine("Size of double: " + sizeof(double));
+            Console.WriteLine("Size of char: " + sizeof(char));
+            Console.WriteLine("Size of bool: " + sizeof(bool));
+        }
+    }
+}
