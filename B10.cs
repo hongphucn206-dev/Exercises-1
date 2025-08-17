dd2959ead222bd97438ff8f2512285cdfc195f67
hongphucn206-dev <hongphucn206@gmail.com>
2025-08-17 12:20:51 +0700

    Add files via upload

---
+++ B10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int years = days / 365;
+            int weeks = (days % 365) / 7;
+            int remainingDays = (days % 365) % 7;
+            Console.WriteLine($"{years} {weeks} {remainingDays}");
+        }
+    }
+}
